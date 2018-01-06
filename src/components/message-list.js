65e94239735f7fff1218af27b49928d9935a1425
Maria Mata <mariajo.mata@gmail.com>
2018-01-05 21:24:23 -0700

    setup component hierarchy

---
+++ src/components/message-list.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Message from './message'
+
+const MessageList = () => {
+  return (
+    <Message />
+  );
+}
+
+export default MessageList;
---
+++ src/components/message.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Message = () => {
+  return (
+    <div className="row message unread">
+      <div className="col-xs-1">
+        <div className="row">
+          <div className="col-xs-2">
+            <input type="checkbox" />
+          </div>
+          <div className="col-xs-2">
+            <i className="star fa fa-star-o"></i>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-11">
+        <a>
+          Here is some message text that has a bunch of stuff
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Message;
